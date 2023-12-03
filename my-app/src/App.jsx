e9fcf149a93fa51560cd169784b0592ba0b9ce02
PurpleNurps <seanlunt1991@gmail.com>
2023-12-03 13:01:03 +0000

    got the ai working

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,12 +2,13 @@ import * as React from 'react'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
+import Form from './components/Form'
 
 export default function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <TheRestOfYourApplication />
+      <Form />
     </ChakraProvider>
   )
 }
