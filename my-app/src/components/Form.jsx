19fac4b3bc312365676af771746a2bf81a5dc834
PurpleNurps <seanlunt1991@gmail.com>
2023-12-04 08:56:46 +0000

    added some styling to the form component

--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -1,10 +1,34 @@
-import { Input } from '@chakra-ui/react'
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
 
 // Create a form component that takes in user input
 // and returns a list of recipes
-// 
+//
 export default function Form() {
-    return (
-        <Input placeholder="Enter ingredients here" />
-    )
+  return (
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}
+      bg="orange.200"
+    >
+      <Center>
+        <Container p="20px" bg="white" borderRadius={10}>
+          <FormControl>
+            <FormLabel>Recipe Bot</FormLabel>
+            <FormHelperText>Hey good looking, what you got cooking?</FormHelperText>
+            <Input placeholder="Enter ingredients here" />
+          </FormControl>
+        </Container>
+      </Center>
+    </Flex>
+  );
 }
